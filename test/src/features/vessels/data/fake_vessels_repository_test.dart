ec01853a7572a722d48f5fb40660d97ded45fbd4
Rob <rob.bebbington@outlook.com>
2022-07-17 16:03:41 +1000

    Changes to be committed: 	new file:   test/src/features/authentication/data/fake_auth_repository_test.dart 	modified:   test/src/features/vessels/data/fake_vessels_repository_test.dart

--- test/src/features/vessels/data/fake_vessels_repository_test.dart
+++ test/src/features/vessels/data/fake_vessels_repository_test.dart
@@ -3,16 +3,41 @@ import 'package:my_vessels/src/constants/test_vessels.dart';
 import 'package:my_vessels/src/features/vessels/data/fake_vessels_repository.dart';
 
 void main() {
-  test('getVesselsList returns global list', () {
-    final vesselsRepository = FakeVesselsRepository();
-    expect(vesselsRepository.getVesselsList(), kTestVessels);
+  FakeVesselsRepository makeVesselsRepository() => FakeVesselsRepository(
+        addDelay: false,
+      );
+  group('FakeProductRepository', () {
+    test('getVesselsList returns global list', () {
+      final vesselsRepository = makeVesselsRepository();
+      expect(vesselsRepository.getVesselsList(), kTestVessels);
+    });
+    test('getVessel(1) returns first vessel', () {
+      final vesselsRepository = makeVesselsRepository();
+      expect(vesselsRepository.getVessel('1'), kTestVessels[0]);
+    });
+    test('getVessel(100) returns null', () {
+      final vesselRepository = makeVesselsRepository();
+      expect(vesselRepository.getVessel('100'), null);
+    });
   });
-  test('getVessel(1) returns first vessel', () {
-    final vesselsRepository = FakeVesselsRepository();
-    expect(vesselsRepository.getVessel('1'), kTestVessels[0]);
+
+  test('fetchVesselsList returns global list', () async {
+    final vesselsRepository = makeVesselsRepository();
+    expect(await vesselsRepository.fetchVesselsList(), kTestVessels);
   });
-  test('getVessel(100) returns null', () {
-    final vesselRepository = FakeVesselsRepository();
-    expect(vesselRepository.getVessel('100'), null);
+
+  test('watchVesselsList emits global list', () {
+    final vesselsRepository = makeVesselsRepository();
+    expect(vesselsRepository.watchVesselsList(), emits(kTestVessels));
+  });
+
+  test('watchVessel(1) emits first item', () {
+    final vesselsRepository = makeVesselsRepository();
+    expect(vesselsRepository.watchVessel('1'), emits(kTestVessels[0]));
+  });
+
+  test('watchVessel(100) emits null', () {
+    final vesselsRepository = makeVesselsRepository();
+    expect(vesselsRepository.watchVessel('100'), emits(null));
   });
 }
